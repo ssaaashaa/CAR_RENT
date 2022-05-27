3dd1c9704a43d46d7cd0759e16e1835b4ff67632
aleksavesna2002@gmail.com <aleksa-vesna@mail.ru>
2022-05-27 12:22:27 +0300

    promoCode

--- CAR_RENT/CAR_RENT/pagesForAdmin/promo_codePage.xaml.cs
+++ CAR_RENT/CAR_RENT/pagesForAdmin/promo_codePage.xaml.cs
@@ -25,97 +25,118 @@ namespace CAR_RENT.pagesForAdmin
         public promo_codePage()
         {
             InitializeComponent();
-            DGridPromocode.ItemsSource = CAR_RENTEntities.GetContext().PROMO_CODE.ToList();
-            DISCOUNT_AMOUNT.PreviewTextInput += new TextCompositionEventHandler(discount_amountTextInput);
+            try
+            {
+                DGridPromocode.ItemsSource = CAR_RENTEntities.GetContext().PROMO_CODE.ToList();
+                DISCOUNT_AMOUNT.PreviewTextInput += new TextCompositionEventHandler(numbers);
+                PROMO_CODE.PreviewTextInput += new TextCompositionEventHandler(lettersAndNumbers);
+
+            }
+            catch { }
+         
+        }
+        void numbers(object sender, TextCompositionEventArgs e)
+        {
+            try
+            {
+                if (!Char.IsDigit(e.Text, 0))
+                {
+                    e.Handled = true; //не обрабатывать введеный символ
+                }
+            }
+            catch { }
+        }
+        void lettersAndNumbers(object sender, TextCompositionEventArgs e)
+        {
+            try
+            {
+                if (!Char.IsLetterOrDigit(e.Text, 0))
+                {
+                    e.Handled = true; //не обрабатывать введеный символ
+                }
+            }
+            catch { }
+        }
+        private void space_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                if (e.Key == Key.Space)
+                {
+                    e.Handled = true;
+                }
+            }
+            catch { }
         }
         public void Clear()
         {
-            PROMO_CODE.Clear();
-            DISCOUNT_AMOUNT.Clear();
+            try
+            {
+                PROMO_CODE.Clear();
+                DISCOUNT_AMOUNT.Clear();
+            }
+            catch { }
         }
         private void Add_Click(object sender, RoutedEventArgs e)
         {
-            StringBuilder errors = new StringBuilder();
-            if (string.IsNullOrWhiteSpace(PROMO_CODE.Text))
-            {
-                errors.AppendLine("Введите промокод!");
-            }
-            if (string.IsNullOrWhiteSpace(DISCOUNT_AMOUNT.Text.ToString()))
-            {
-                errors.AppendLine("Введите величину скидки!");
-            }
-            if (errors.Length > 0)
-            {
-                MessageBox.Show(errors.ToString());
-                return;
-            }
-            PROMO_CODE currentPromo_code = new PROMO_CODE();
-            currentPromo_code.PROMO_CODE1 = PROMO_CODE.Text;
-            currentPromo_code.DISCOUNT_AMOUNT = DISCOUNT_AMOUNT.Text;
-            CAR_RENTEntities.GetContext().PROMO_CODE.Add(currentPromo_code);
+          
             try
             {
+                StringBuilder errors = new StringBuilder();
+                if (string.IsNullOrWhiteSpace(PROMO_CODE.Text))
+                {
+                    errors.AppendLine("Введите промокод!");
+                }
+                if (string.IsNullOrWhiteSpace(DISCOUNT_AMOUNT.Text.ToString()))
+                {
+                    errors.AppendLine("Введите величину скидки!");
+                }
+                PROMO_CODE currentPromocode = CAR_RENTEntities.GetContext().PROMO_CODE.Where(u => u.PROMO_CODE1 == PROMO_CODE.Text).FirstOrDefault();
+                if(currentPromocode != null)
+                {
+                    errors.AppendLine("Такой промокод уже существует!");
+                }
+                if (errors.Length > 0)
+                {
+                    MessageBox.Show(errors.ToString());
+                    return;
+                }
+                PROMO_CODE currentPromo_code = new PROMO_CODE();
+                currentPromo_code.PROMO_CODE1 = PROMO_CODE.Text;
+                currentPromo_code.DISCOUNT_AMOUNT = DISCOUNT_AMOUNT.Text;
+                CAR_RENTEntities.GetContext().PROMO_CODE.Add(currentPromo_code);
                 CAR_RENTEntities.GetContext().SaveChanges();
                 DGridPromocode.ItemsSource = CAR_RENTEntities.GetContext().PROMO_CODE.ToList();
                 Clear();
                 MessageBox.Show("Данные успешно добавлены!");
             }
-            catch 
+            catch (DbEntityValidationException ex)
             {
-                MessageBox.Show("Такая запись уже существует!");
+              
+                foreach (DbEntityValidationResult validationError in ex.EntityValidationErrors)
+                {
+                    foreach (DbValidationError err in validationError.ValidationErrors)
+                    {
+                        MessageBox.Show(err.ErrorMessage + " ");
+
+                    }
+                }
             }
         }  
-        void discount_amountTextInput(object sender, TextCompositionEventArgs e)
-        {
-            if (!Char.IsDigit(e.Text, 0))
-            {
-                e.Handled = true; //не обрабатывать введеный символ
-            }
-        }
-        private void Edit_Click(object sender, RoutedEventArgs e)
-        {
-            if (PROMO_CODE.Text.Equals(""))
-            {
-                MessageBox.Show("Проверьте введенные данные либо выделите запись, которую требуется изменить!");
-            }
-            try
-            {
-                PROMO_CODE currentPromocode = CAR_RENTEntities.GetContext().PROMO_CODE.Where(u => u.PROMO_CODE1 == PROMO_CODE.Text).FirstOrDefault();
-                currentPromocode.PROMO_CODE1 = PROMO_CODE.Text;
-                currentPromocode.DISCOUNT_AMOUNT = DISCOUNT_AMOUNT.Text;
-                if (currentPromocode != null)
-                {
-                    try
-                    {
-                        CAR_RENTEntities.GetContext().SaveChanges();
-                        DGridPromocode.ItemsSource = CAR_RENTEntities.GetContext().PROMO_CODE.ToList();
-                        Clear();
-                        MessageBox.Show("Данные успешно обновлены!");
-                    }
-                    catch
-                    {
-                        MessageBox.Show("Необходимо выбрать запись для редактирования!");
-                    }
-                }
-
-            }
-            catch
-            {
-                if (string.IsNullOrEmpty(PROMO_CODE.Text) == false)
-                {
-                    MessageBox.Show("Такого промокода не существует!");
-                }
-            }
-        }
+      
+       
        
         private void Delete_Click(object sender, RoutedEventArgs e)
         {
-            if (PROMO_CODE.Text.Equals(""))
+            try
             {
-                MessageBox.Show("Проверьте введенные данные либо выделите запись, которую требуется удалить!");
-            }
+                if (PROMO_CODE.Text.Equals(""))
+                {
+                    MessageBox.Show("Выделите запись, которую требуется удалить!");
+                }
                 PROMO_CODE currentPromocode = CAR_RENTEntities.GetContext().PROMO_CODE.Where(p => p.PROMO_CODE1 == PROMO_CODE.Text).FirstOrDefault();
-                if (currentPromocode != null)
+                var contract = CAR_RENTEntities.GetContext().CONTRACTS.Where(c => c.PROMO_CODE == PROMO_CODE.Text).ToList();
+                if (currentPromocode != null && contract.Count==0)
                 {
                     CAR_RENTEntities.GetContext().PROMO_CODE.Remove(currentPromocode);
                     try
@@ -130,10 +151,17 @@ namespace CAR_RENT.pagesForAdmin
                         MessageBox.Show(ex.Message);
                     }
                 }
-                else if(currentPromocode == null&&string.IsNullOrEmpty(PROMO_CODE.Text)==false)
+                if(currentPromocode != null && contract.Count>=1)
+                {
+                    MessageBox.Show("Удаление промокода и и нформации о нем невозможно!");
+
+                }
+                else if (currentPromocode == null && string.IsNullOrEmpty(PROMO_CODE.Text) == false)
                 {
                     MessageBox.Show("Такого промокода не существует!");
-                }           
+                }
+            }
+            catch { }
         }
 
         private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
